9b4be507f09bb8d53b0797f44cb1c73771b87612
Daewii <mateomine007@gmail.com>
2022-08-14 11:47:46 -0500

    header v.1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { get } = require('http');
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
