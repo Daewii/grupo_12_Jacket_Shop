822ba58c74e97ca595dabe74f67bc72f8e97f889
martesdieciocho <alejo.9418@hotmail.com>
2022-12-03 15:15:31 -0500

    Vista para registro y estilos

--- database/config/config.js
+++ database/config/config.js
@@ -4,14 +4,16 @@ module.exports ={
     "password": null,
     "database": "jacket_shop_db",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": "3001"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "jacket_shop_db",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "port": "3001"
   },
   "production": {
     "username": "root",
