49908c579593b3023ecb923301891689599fd631
MarcelPineros <mpineross25@gmail.com>
2022-12-10 16:19:32 -0500

    se realizan las validaciones en createValidation.js y createProductsValudator.js, en este segundo estan dos validaciones diferentes, la sencilla y la no tan sencilla dividida en validaciones y validaciones 2, la 2 se usa con el createValidation, la 1 va sin este archivo

---
+++ public/js/createValidation.js
@@ -0,0 +1,61 @@
+const productsCreateForms = document.querySelector("#productsCreateForms");
+
+const productName = document.querySelector("[name=nameProduct]")
+const productDescription = document.querySelector("[name=description]")
+const productImage = document.querySelector("[name=image]")
+const productCategory = document.querySelector("[name=category_id]")
+const productMaterial = document.querySelector("[name=material_id]")
+const productColors = document.querySelector("[name=colors_id]")
+const productSizes = document.querySelector("[name=sizes_id]")
+const productPrice = document.querySelector("[name=price]")
+
+
+const validateEmptyField = (e) =>{
+    const field = e.target;
+    if (field.value.trim() === ""){
+        const spanTagError = field.nextElementSibling;
+        spanTagError.innerHTML = "</br> El campo " + field.name + " es obligatorio";
+        spanTagError.classList.add("is-invalid");
+        field.classList.add("alert-warning");        
+    } else {
+        const spanTagError = field.nextElementSibling;
+        spanTagError.innerText = "";
+        spanTagError.classList.remove("is-invalid");
+        field.classList.remove("alert-warning");
+    }
+}
+
+productName.addEventListener("blur", validateEmptyField)
+productDescription.addEventListener("blur", validateEmptyField)
+productImage.addEventListener("blur", validateEmptyField)
+productCategory.addEventListener("blur", validateEmptyField)
+productMaterial.addEventListener("blur", validateEmptyField)
+productColors.addEventListener("blur", validateEmptyField)
+productSizes.addEventListener("blur", validateEmptyField)
+productPrice.addEventListener("blur", validateEmptyField)
+
+productsCreateForms.addEventListener("submit", function(e){
+    let error = false;
+    formFields = [productsCreateForms.elements];
+    console.log(formFields);
+    formFields.pop();
+    
+    formFields.forEach(oneField =>{
+        if (oneField.value.trim() === ""){
+            const spanTagError = oneField.nextElementSibling;
+            spanTagError.innerHTML = "</br> El campo " + oneField.name + " es obligatorio";
+            spanTagError.classList.add("is-invalid");
+            oneField.classList.add("alert-warning"); 
+            error = true;       
+        } else {
+            const spanTagError = oneField.nextElementSibling;
+            spanTagError.innerText = "";
+            spanTagError.classList.remove("is-invalid");
+            oneField.classList.remove("alert-warning");
+            }
+    })
+    if(error){
+        e.preventDefault();  // Evitamos que se envíe el form
+        console.log("El formulario no se va a enviar");
+    }
+})
